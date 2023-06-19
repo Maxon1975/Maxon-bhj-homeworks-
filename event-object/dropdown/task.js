f913233b0164062f4484e7f226c2c3ee847fa9e0
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2023-06-19 12:44:18 +0300

    Update task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,22 +1,19 @@
-const dropdownValue = document.querySelector(".dropdown__value");
-const dropdownList = document.querySelector(".dropdown__list");
-dropdownValue.onclick = function () {
-    if (dropdownList.classList == 'dropdown__list dropdown__list_active') {
-        dropdownList.classList.remove('dropdown__list_active');
-    } else {
-        dropdownList.classList.add('dropdown__list_active');
-    }
-}
-const dropdownItem = document.body.querySelectorAll(".dropdown__item");
-for (let i = 0; i < dropdownItem.length; i++) {
-    dropdownItem[i].addEventListener("click", function (event) {
-        const er = this.closest('.dropdown__list');
-        if (er.classList[0] == 'dropdown__list') {
-            event.target.onclick = function () {
-                return false;
-            }
-            dropdownValue.textContent = event.target.textContent;
-            dropdownList.classList.remove('dropdown__list_active');
-        }
-    });
-}
+const dLinks = Array.from(document.querySelectorAll(".dropdown__link"));
+const dLists = document.querySelector(".dropdown__list");
+const dValue = document.querySelector(".dropdown__value");
+
+
+dValue.addEventListener("click", () => {
+    dLists.classList.add("dropdown__list_active");
+})
+
+
+dLinks.forEach((element) => {
+
+    element.addEventListener("click", function (ev) {
+        dValue.textContent = element.textContent;
+        dLists.classList.remove("dropdown__list_active");
+        ev.preventDefault()
+    })
+})
+   
